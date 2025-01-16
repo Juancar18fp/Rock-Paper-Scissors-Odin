c2614d87f32effebb24fb369b08272ff74c2cd7d
Juancar18fp <38106370+Juancar18fp@users.noreply.github.com>
2025-01-16 11:34:54 +0100

    Add three buttons and remove all the prompt interaction
    Added three buttons for choice rock, paper or scissors insted of wirting
    in a prompt and removed all the console.log() for display the round winner
    Also added two scores: one for the cpu and the other for the player. When
    you press a button the cpu choice between rock, paper and scissors randomly.
    If you win the round a text appears and add one to the score and vice versa.

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const PIEDRA = "PIEDRA";
+const PAPEL = "PAPEL";
+const TIJERA = "TIJERA";
+
 function getComputerChoice() {
     let n = Math.floor(Math.random() * 3);
     let str = ""
     if (n === 0) {
-        return "PIEDRA";
+        return PIEDRA;
     } else if (n === 1) {
-        return "PAPEL";
+        return PAPEL;
     } else {
-        return "TIJERA";
+        return TIJERA;
     }
 }
 
 function getHumanChoice() {
     let op = prompt("Introduce piedra, papel o tijera. (piedra por defecto)");
     op = op.toUpperCase();
-    if (op != "PIEDRA" && op != "PAPEL" && op != "TIJERA") {
-        return "PIEDRA"
+    if (op != PIEDRA && op != PAPEL && op != TIJERA) {
+        return PIEDRA
     } else {
         return op;
     }
 }
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "PIEDRA" && computerChoice === "TIJERA" ||
-        humanChoice === "PAPEL" && computerChoice === "PIEDRA" ||
-        humanChoice === "TIJERA" && computerChoice === "PAPEL") {
-        console.log(`Has ganado la ronda! ${humanChoice} gana a ${computerChoice}`)
-        humanScore++;
-    }
-    else if (computerChoice === "PIEDRA" && humanChoice === "TIJERA" ||
-        computerChoice === "PAPEL" && humanChoice === "PIEDRA" ||
-        computerChoice === "TIJERA" && humanChoice === "PAPEL") {
-        console.log(`Has perdido la ronda! ${humanChoice} pierde contra ${computerChoice}`)
-        computerScore++;
+    const text = document.querySelector(".winner-text");
+    if (computerScore >= 5 || humanScore >= 5) {
+        
     } else {
-        console.log("Has empatado la ronda!!!")
+        if (humanChoice === PIEDRA && computerChoice === TIJERA ||
+            humanChoice === PAPEL && computerChoice === PIEDRA ||
+            humanChoice === TIJERA && computerChoice === PAPEL) {
+            text.textContent = `Has ganado la ronda!`;
+            humanScore++;
+        }
+        else if (computerChoice === PIEDRA && humanChoice === TIJERA ||
+            computerChoice === PAPEL && humanChoice === PIEDRA ||
+            computerChoice === TIJERA && humanChoice === PAPEL) {
+            text.textContent = `Has perdido la ronda!`;
+            computerScore++;
+        } else {
+            text.textContent = "Has empatado la ronda!!!";
+        }
+        const playerScore = document.querySelector(".player-score-num");
+        const cpuScore = document.querySelector(".cpu-score-num");
+        playerScore.textContent = humanScore;
+        cpuScore.textContent = computerScore;
     }
 }
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+        if (e.target.matches("div#rockBtn.sign")) {
+            playRound(PIEDRA, getComputerChoice());
+        } else if (e.target.matches("div#paperBtn.sign")) {
+            playRound(PAPEL, getComputerChoice());
+        } else {
+            playRound(TIJERA, getComputerChoice());
+        }
+    });
+});
